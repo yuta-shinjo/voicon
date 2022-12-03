8615709a970ad3f6f70e246791514630a09a6055
yuta_shin <dsisk56gh@gmail.com>
2022-12-04 00:57:07 +0900

    Initial Commit

---
+++ voicon/voiconApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiconApp.swift
+//  voicon
+//
+//  Created by 新庄勇太 on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct voiconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
