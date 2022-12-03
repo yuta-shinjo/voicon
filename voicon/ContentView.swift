1d6ae5e05ae672211f1a93df94938b026c93f281
yuta_shin <dsisk56gh@gmail.com>
2022-12-04 01:36:45 +0900

    検索バーを追加

--- voicon/ContentView.swift
+++ voicon/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    let spots = ["東京都美術館","国立新美術館","国立近代美術館","東京国立博物館","江戸東京博物館","国立科学博物館","新江ノ島水族館","川崎水族館","しながわ水族館"]
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(searchResults, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            .searchable(text: $searchText, prompt: "動画を検索")
+            .keyboardType(.default)
+            .navigationTitle("人気の動画")
         }
-        .padding()
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return spots
+        } else {
+            return spots.filter{ $0.contains(searchText) }
+        }
     }
 }
 
